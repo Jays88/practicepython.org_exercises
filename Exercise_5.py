f37d9e6ae4acb14f10fd17c767ceeef08f467f37
Janek Křikava <39810520+Jays88@users.noreply.github.com>
2018-06-01 14:13:36 +0100

    Add files via upload
    This is my take at an exercise #5. The task was:
    Take two lists, say for example these two:
    
      a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
      b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
    and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.

---
+++ Exercise_5.py
@@ -0,0 +1,9 @@
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+c = []
+
+for n in b:
+    if n in a:
+        c.append(n)    
+
+print("My first unique list", c)
